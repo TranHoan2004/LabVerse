f81eec41db72fcb1ecaf9e9be8e9afcd3134c33e
TranHoan <hoantxhe180030@fpt.edu.vn>
2025-09-21 22:32:15 +0700

    feat: add necessary dependencies

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    id("io.freefair.lombok") version "8.14.2"
 }
 
 android {
@@ -37,6 +38,33 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.recyclerview)
+    implementation(libs.cardview)
+
+    implementation(libs.room.runtime)
+    compileOnly(libs.lombok.v11830)
+    compileOnly(libs.room.compiler)
+    annotationProcessor(libs.lombok.v11830)
+    annotationProcessor(libs.room.compiler)
+
+    implementation(libs.gson)
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.gson)
+    implementation(libs.okhttp)
+    implementation(libs.logging.interceptor)
+
+    implementation(libs.glide)
+    annotationProcessor(libs.glide.compiler)
+
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.analytics)
+    implementation(libs.firebase.storage)
+    implementation(libs.firebase.messaging)
+
+    implementation(libs.android.pdf.viewer)
+
+    implementation(libs.lottie)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)

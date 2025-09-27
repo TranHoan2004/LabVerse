c2d27e83544505a8f1432c040003485c64d46379
TranHoan <hoantxhe180030@fpt.edu.vn>
2025-09-27 23:35:26 +0700

    fix: add h2 database in order to exclude MySQL when building in CI

--- services/ReferenceService/src/test/java/com/se1853_jv/Se1853JvApplicationTests.java
+++ services/ReferenceService/src/test/java/com/se1853_jv/Se1853JvApplicationTests.java
@@ -1,16 +1,11 @@
 package com.se1853_jv;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@EnableAutoConfiguration(exclude = {
-        DataSourceAutoConfiguration.class,
-        HibernateJpaAutoConfiguration.class
-})
+@ActiveProfiles("test")
 class Se1853JvApplicationTests {
 
     @Test

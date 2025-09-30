077198f98d72d4075afdc07efae2ee936cff58db
tuanpa79 <tuanpa79@fpt.com>
2025-09-30 20:58:11 +0700

    feat: initialize Eureka Server

---
+++ services/EurekaSever/src/main/java/com/se1853_jv/EurekaSeverApplication.java
@@ -0,0 +1,17 @@
+package com.se1853_jv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+@EnableDiscoveryClient
+public class EurekaSeverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaSeverApplication.class, args);
+	}
+
+}
